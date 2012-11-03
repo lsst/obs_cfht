7cbe74ca45b1b805c82be26871674b6033fbeb3c
Paul Price <price@astro.princeton.edu>
2012-11-03 17:56:20 -0400

    First attempt at ingestion, not tested yet.

---
+++ config/ingest.py
@@ -0,0 +1,24 @@
+from lsst.obs.cfht.ingest import MegacamParseTask
+root.parse.retarget(MegacamParseTask)
+root.parse.hdu = 1 # PHU
+root.parse.translation = {'runId': 'RUNID',
+                          'object': 'OBJECT',
+                          'exp': 'EXPNUM',
+                          'date': 'DATE-OBS',
+                          'expTime': 'EXPTIME',
+                          'filter': 'FILTER',
+                          }
+root.parse.translators = {'taiObs': 'translate_taiObs',
+                          'ccd': 'translate_ccd',
+                          }
+root.parse.extnames = ["ccd%02d" % ccd for ccd in range(36)]
+root.register.columns = {'runId':    'text',
+                         'object':   'text',
+                         'exp':      'int',
+                         'ccd':      'int',
+                         'filter':   'text',
+                         'date':     'text',
+                         'taiObs':   'text',
+                         'expTime':  'double',
+                         }
+root.register.unique = ['visit', 'ccd',]
