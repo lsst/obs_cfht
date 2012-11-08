55454673d87e21900f4eb7fe6dfee1f5462d1302
Paul Price <price@astro.princeton.edu>
2012-11-07 20:55:34 -0500

    Add fringe subtraction.

--- config/processCcd.py
+++ config/processCcd.py
@@ -4,9 +4,13 @@ root.isr.retarget(MegacamIsrTask)
 root.isr.doBias = True
 root.isr.doDark = False
 root.isr.doFlat = True
+root.isr.doFringe = True
+root.isr.fringeAfterFlat = True
 root.isr.doWrite = False
-#root.isr.setGainAssembledCcd = False
+root.isr.setGainAssembledCcd = True
 root.isr.assembleCcd.doRenorm = False
 root.isr.assembleCcd.setGain = False
+root.isr.fringe.filters = ['i', 'i2', 'z']
+root.isr.fringe.pedestal = True
 
-root.calibrate.repair.cosmicray.nCrPixelMax = 1000000
+root.calibrate.repair.cosmicray.nCrPixelMax = 100000
