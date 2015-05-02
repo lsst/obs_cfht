01d30315d17fbac6e01c7bfe32d9a9be7873cb1e
Russell Owen <rowen@uw.edu>
2015-05-01 17:49:12 -0700

    Simplify CfhtIsrTask by resuing most of the default implementation
    The only reason CfhtIsrTask exists is to override how saturation is handled.
    This simpler variant task starts out by updating the amp info, then
    uses IsrTask to perform all other processing.
    As a result there is one minor duplication of effort: converting the data
    from int to float.

--- python/lsst/obs/cfht/cfhtIsrTask.py
+++ python/lsst/obs/cfht/cfhtIsrTask.py
@@ -1,6 +1,4 @@
-import lsst.afw.image as afwImage
 import lsst.pex.config as pexConfig
-import lsst.pipe.base as pipeBase
 from lsst.ip.isr import IsrTask
 import numpy as np
 
@@ -36,27 +34,15 @@ class CfhtIsrTask(IsrTask) :
         @return a pipeBase.Struct with fields:
         - exposure: the exposure after application of ISR
         """
-        #Validate Input
-        if self.config.doBias and bias is None:
-            raise RuntimeError("Must supply a bias exposure if config.doBias True")
-        if self.config.doDark and dark is None:
-            raise RuntimeError("Must supply a dark exposure if config.doDark True")
-        if self.config.doFlat and flat is None:
-            raise RuntimeError("Must supply a flat exposure if config.doFlat True")
-        if self.config.doFringe and fringes is None:
-            raise RuntimeError("Must supply fringe list or exposure if config.doFringe True")
-
-        defects = [] if defects is None else defects
-
         ccd = ccdExposure.getDetector()
-        ccdExposure = self.convertIntToFloat(ccdExposure)
-        metadata = ccdExposure.getMetadata()
+        floatExposure = self.convertIntToFloat(ccdExposure)
+        metadata = floatExposure.getMetadata()
         
         # Detect saturation
         # Saturation values recorded in the fits hader is not reliable, try to estimate it from the pixel vales
         # Find the peak location in the high end part the pixel values' histogram and set the saturation level at 
         # safe * (peak location) where safe is a configurable parameter (typically 0.95)
-        image = ccdExposure.getMaskedImage().getImage()
+        image = floatExposure.getMaskedImage().getImage()
         imageArray = image.getArray()
         maxValue = np.max(imageArray)
         if maxValue > 60000.0 :
@@ -76,40 +62,12 @@ class CfhtIsrTask(IsrTask) :
                 amp.setReadNoise(metadata.get("RDNOISEB"))
             else :
                 raise ValueError("Unexpected amplifier name : %s"%(amp.getName()))
-            
-            self.saturationDetection(ccdExposure, amp)
-            self.overscanCorrection(ccdExposure, amp)
-        
-        if self.config.doAssembleCcd:
-            ccdExposure = self.assembleCcd.assembleCcd(ccdExposure)
 
-        if self.config.doBias:
-            self.biasCorrection(ccdExposure, bias)
-
-        if self.config.doDark:
-            self.darkCorrection(ccdExposure, dark)
-        
-        for amp in ccd:
-            ampExposure = ccdExposure.Factory(ccdExposure, amp.getBBox(), afwImage.PARENT)
-            self.updateVariance(ampExposure, amp)
-
-        if self.config.doFringe and not self.config.fringeAfterFlat:
-            self.fringe.removeFringe(ccdExposure, fringes)
-
-        if self.config.doFlat:
-            self.flatCorrection(ccdExposure, flat)
-
-        self.maskAndInterpDefect(ccdExposure, defects)
-
-        self.saturationInterpolation(ccdExposure)
-
-        self.maskAndInterpNan(ccdExposure)
-
-        if self.config.doFringe and self.config.fringeAfterFlat:
-            self.fringe.removeFringe(ccdExposure, fringes)
-        
-        ccdExposure.getCalib().setFluxMag0(self.config.fluxMag0T1 * ccdExposure.getCalib().getExptime())
-
-        return pipeBase.Struct(
-            exposure = ccdExposure,
+        return IsrTask.run(self, 
+            ccdExposure = ccdExposure,
+            bias = bias,
+            dark = dark,
+            flat = flat,
+            defects = defects,
+            fringes = fringes,
         )
