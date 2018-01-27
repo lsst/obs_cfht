6e52dda9fa2877704c7d8b3eb53d6bca14c8ab22
Jim Bosch <jbosch@astro.princeton.edu>
2018-01-27 10:16:00 -0500

    Forward extra keyword arguments to IsrTask.run()
    This insulates the CFHT specialization from additions
    IsrTask.run's signature (such as the TransmissionCurves
    just added on DM-12375).

--- python/lsst/obs/cfht/cfhtIsrTask.py
+++ python/lsst/obs/cfht/cfhtIsrTask.py
@@ -20,7 +20,7 @@ class CfhtIsrTask(IsrTask):
     ConfigClass = CfhtIsrTaskConfig
 
     def run(self, ccdExposure, bias=None, linearizer=None, dark=None, flat=None, defects=None,
-            fringes=None, bfKernel=None):
+            fringes=None, bfKernel=None, **kwds):
         """Perform instrument signature removal on an exposure
 
         Steps include:
@@ -37,6 +37,7 @@ class CfhtIsrTask(IsrTask):
         @param[in] defects -- list of detects
         @param[in] fringes -- exposure of fringe frame or list of fringe exposure
         @param[in] bfKernel - kernel used for brighter-fatter correction; currently unsupported
+        @param[in] **kwds     additional kwargs forwarded to IsrTask.run.
 
         @return a pipeBase.Struct with fields:
         - exposure: the exposure after application of ISR
@@ -94,4 +95,5 @@ class CfhtIsrTask(IsrTask):
                            flat=flat,
                            defects=defects,
                            fringes=fringes,
+                           **kwds
                            )
