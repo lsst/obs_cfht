1364e0870a352a31a3e7f2b2050d3780bff514af
Michael Wood-Vasey <wmwv@pitt.edu>
2018-10-31 20:12:42 -0500

    Use same color terms for any PS1 catalog, not just ps1_pv3_3pi_20170110.

--- config/colorterms.py
+++ config/colorterms.py
@@ -2,8 +2,13 @@
 
 from lsst.pipe.tasks.colorterms import Colorterm, ColortermDict
 
+# 2018-09-04: Michael Wood-Vasey:
+# The color terms should translate between the photometric reference catalog
+# and the system transmission of MegaCam.
+# I've added a set of color terms here for the PS1 reference catalog
+# assuming that the PS1 catalog is on the AB system.
 config.data = {
-    "ps1_pv3_3pi_20170110": ColortermDict(data={
+    "ps1*": ColortermDict(data={
         "u": Colorterm(primary="u", secondary="g", c0=0.0, c1=-0.241),
         "g": Colorterm(primary="g", secondary="r", c0=0.0, c1=-0.153),
         "r": Colorterm(primary="r", secondary="g", c0=0.0, c1=0.024),
