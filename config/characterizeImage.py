4e65d9c3cdb46e09e43dba70d75e8639d03e8397
John Parejko <parejkoj@uw.edu>
2021-11-12 16:54:42 -0800

    cosmic ray repair defaults that work with validation_data_cfht
    These are not vetted on any other CFHT data.

--- config/characterizeImage.py
+++ config/characterizeImage.py
@@ -3,10 +3,8 @@
 config.refObjLoader.filterMap = {'i2': 'i'}
 config.refObjLoader.ref_dataset_name = "ps1_pv3_3pi_20170110"
 
-config.repair.doCosmicRay = True
-config.repair.cosmicray.cond3_fac = 2.5
-config.repair.cosmicray.cond3_fac2 = 0.4
-config.repair.cosmicray.niteration = 3
+# These cosmic ray defaults work for validation_data_cfht (which is post-ISR
+# data, thus not actually raw); it may not be correct for all CFHT data.
+config.requireCrForPsf = False
 config.repair.cosmicray.nCrPixelMax = 100000
-config.repair.cosmicray.minSigma = 6.0
-config.repair.cosmicray.min_DN = 150.0
+config.repair.cosmicray.min_DN = 3000
