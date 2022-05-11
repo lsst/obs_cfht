c441dad7529a6c6827c79ca30968efcc23373c74
John Parejko <parejkoj@uw.edu>
2022-05-11 12:36:40 -0700

    Remove deprecated refcat config

--- config/characterizeImage.py
+++ config/characterizeImage.py
@@ -1,8 +1,5 @@
 # characterize image task config for CFHT single frame measurement
 
-config.refObjLoader.filterMap = {'i2': 'i'}
-config.refObjLoader.ref_dataset_name = "ps1_pv3_3pi_20170110"
-
 # These cosmic ray defaults work for validation_data_cfht (which is post-ISR
 # data, thus not actually raw); it may not be correct for all CFHT data.
 config.requireCrForPsf = False
