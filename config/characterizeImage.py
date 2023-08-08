fbdc37722d5905ee98ade219ef4d2c30e2619164
John Parejko <parejkoj@uw.edu>
2023-08-07 17:59:12 -0700

    Remove now-defaulted overrides
    This commit is not intended to change pipeline behavior: the combined effect
    of this branch across all packages is to cleanup our configs so that the task
    defaults are what we believe to be most appropriate for most cases.

--- config/characterizeImage.py
+++ config/characterizeImage.py
@@ -3,5 +3,4 @@
 # These cosmic ray defaults work for validation_data_cfht (which is post-ISR
 # data, thus not actually raw); it may not be correct for all CFHT data.
 config.requireCrForPsf = False
-config.repair.cosmicray.nCrPixelMax = 100000
 config.repair.cosmicray.min_DN = 3000
